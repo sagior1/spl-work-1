afa65d8d0bfbc46f1012da66afaf39b624babebf
sagior1 <157025263+sagior1@users.noreply.github.com>
2024-01-31 22:43:52 +0200

    Update main.cpp

--- spl-work-1/spl-work-1/Skeleton/src/main.cpp
+++ spl-work-1/spl-work-1/Skeleton/src/main.cpp
@@ -8,35 +8,15 @@ using namespace std;
 WareHouse* backup = nullptr;
 
 int main(int argc, char** argv){
-//    if(argc!=2){
-//        std::cout << "usage: warehouse <config_path>" << std::endl;
-//        return 0;
-//    }
-    //tests
-    cout << "its happaning" << endl;
-    //tests
+   if(argc!=2){
+       std::cout << "usage: warehouse <config_path>" << std::endl;
+       return 0;
+   }
     
     
-    string configurationFile = "/home/users/bsc/sagior/Downloads/configFileExample.txt";
-    //string configurationFile = argv[1];
+    string configurationFile = argv[1];
     WareHouse wareHouse(configurationFile);
-    wareHouse.start();
-        //tests
-    AddOrder o1(0);
-    AddOrder o2(0);
-    AddOrder o3(0);
-    PrintCustomerStatus customerMoshe(0);
-    o1.act(wareHouse);
-    o2.act(wareHouse);
-    customerMoshe.act(wareHouse);
 
-    PrintOrderStatus pos(0);
-    pos.act(wareHouse);
-    // o3.act(wareHouse);
-    // customerMoshe.act(wareHouse);
-
-    //here the program should throw an error because the first customer has only 2 orders (moshe)
-    cout << "\n good" << endl;
     
     
     //tests
